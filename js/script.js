8f598458305b52db3cee425d2801c0cbf92fceb6
Folake Osibodu <folakeosibodu@gmail.com>
2015-11-07 22:44:49 -0500

    changed overall layout, added hamburger menu, css changes

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $(".hamburger-menu").click(function () {
+        $(".nav-menu").toggle();
+        console.log("it worked");
+    });
+
+
+    console.log("Ready");
+});
